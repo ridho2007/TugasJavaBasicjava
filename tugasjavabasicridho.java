a7514bd11299f8c1854d1cb00520ccfd5b117118
ridho2007 <ridhosalasa534@gmail.com>
2022-11-09 11:15:50 +0700

    anyar

--- tugasjavabasicridho.java
+++ tugasjavabasicridho.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-public class tugasjavabasic {
+public class tugasjavabasicridho {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sandinama = "Administrator";
